42e5bcc918fe4d19c7fc7baf5c67ec7d6be6f343
liuhaichuan <lhc.indie@gmail.com>
2016-03-04 13:57:21 +0800

    init

---
+++ HCSpringBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCSpringBoard
+//
+//  Created by 刘海川 on 16/3/4.
+//  Copyright © 2016年 Haichuan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCSpringBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCSpringBoard
+//
+//  Created by 刘海川 on 16/3/4.
+//  Copyright © 2016年 Haichuan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
